4ddd4eff5d29747bb020e8f80ac6224d47abd40a
Mark Toda <toda.mark@gmail.com>
2022-10-20 17:46:52 -0700

    feat: add balance filterer

---
+++ tools/src/filter_creators_by_balance.rs
@@ -0,0 +1,44 @@
+use ethers::prelude::*;
+use ethers::types::{H160, U256};
+use ethers_providers::{Http, Provider};
+use std::collections::HashMap;
+use std::fs::File;
+use std::str::FromStr;
+use structopt::StructOpt;
+
+mod opt;
+use crate::opt::Opt;
+mod util;
+use crate::util::load_creator_map;
+
+// num workers
+const FILE_NAME: &str = "../data/creators.json";
+
+// some false positives for some reason.. filter by tx count
+#[tokio::main]
+async fn main() {
+    let opt = Opt::from_args();
+    let creator_map = load_creator_map(FILE_NAME);
+    let provider = Provider::<Http>::try_from(opt.rpc_url).expect("Could not create provider");
+    let one_ether = U256::from_dec_str("1000000000000000000").unwrap();
+
+    let mut filtered_map: HashMap<String, usize> = HashMap::new();
+    for (creator, count) in creator_map {
+        // fetch the creator balance
+        let balance = provider
+            .get_balance(
+                H160::from_str(&creator).expect("Could not parse address"),
+                None,
+            )
+            .await
+            .expect("Could not fetch balance");
+        println!("{}: {}", creator, balance);
+        if balance < one_ether {
+            println!("Found one with balance less than one eth");
+            filtered_map.insert(creator, count);
+        }
+    }
+
+    let file = File::create(FILE_NAME).expect("Could not create file");
+    serde_json::to_writer_pretty(file, &filtered_map).expect("Could not write to file");
+}
